43b66a5999eefb9f419a62e353a9adf2d14abfb0
Aliaksey Kandratsenka <alk@tut.by>
2011-04-18 13:43:11 -0700

    initial

---
+++ infect.js
@@ -0,0 +1,64 @@
+// javascript:(function(d,s1,h){s1=d.createElement('script');s1.setAttribute('src','http://lh-alt:9000/js/infect.js');alkInfectURL="http://lh-alt:9000/vbuckets-vis.html";h=d.querySelector('body');h.appendChild(s1);})(document);
+
+//__f=function(prop,propV,v){v=document.createElement('script');v.setAttribute('src',src);return(v)}; document.createElement('script');_vis_s.setAttribute('src', 'http://lh-alt:9000/js/inject.js')document.scrdocument.querySelector("head").appendChild(
+
+// javascript:(function(){readConvertLinksToFootnotes=false;readStyle='style-newspaper';readSize='size-medium';readMargin='margin-narrow';_readability_script=document.createElement('script');_readability_script.type='text/javascript';_readability_script.src='http://lab.arc90.com/experiments/readability/js/readability.js?x='+(Math.random());document.documentElement.appendChild(_readability_script);_readability_css=document.createElement('link');_readability_css.rel='stylesheet';_readability_css.href='http://lab.arc90.com/experiments/readability/css/readability.css';_readability_css.type='text/css';_readability_css.media='all';document.documentElement.appendChild(_readability_css);_readability_print_css=document.createElement('link');_readability_print_css.rel='stylesheet';_readability_print_css.href='http://lab.arc90.com/experiments/readability/css/readability-print.css';_readability_print_css.media='print';_readability_print_css.type='text/css';document.getElementsByTagName('head')[0].appendChild(_readability_print_css);})();
+
+function alkDoInfect(frameURL) {
+  window.alkInfectURL = null;
+  window.addEventListener("message", recvMessage, false);
+  var frameElement = document.createElement("iframe");
+  frameElement.setAttribute("style", "position:absolute;left:0;top:0;width:100%;height:100%;background:white;");
+  document.body.appendChild(frameElement);
+  frameElement.setAttribute('src', frameURL);
+  var frame = frameElement.contentWindow;
+  setTimeout(function () {
+    frame.postMessage("initial", frameURL);
+  }, 100);
+
+  var service = {
+    "eval": function () {
+      frame.postMessage(eval(arguments[0]), frameURL);
+    },
+    "eval2": function () {
+      (function (f, args) {
+        function postMsg(data) {
+          frame.postMessage(data, frameURL);
+        }
+        f.apply(null, [postMsg].concat(Array.prototype.slice.call(args, 1)));
+      })(eval("("+arguments[0]+")"), arguments);
+    },
+    "xhr": function (openArgs, sendArg, headers, responseHeaders) {
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState != 4)
+          return;
+        var reply = [xhr.status,
+                     xhr.text,
+                     (responseHeaders || []).map(function (h) {
+                       return xhr.getResponseHeader(h);
+                     })];
+        frame.postMessage(reply, frameURL);
+      }
+      xhr.open.apply(xhr, openArgs);
+      for (var k in (headers || {})) {
+        xhr.setRequestHeader(k, headers[k]);
+      }
+      xhr.send();
+    }
+  };
+
+  function recvMessage(event) {
+    if (event.source != frame)
+      return;
+
+    var data = event.data;
+    if (!(data instanceof Array)) {
+      return;
+    }
+    console.log("obeying command from master: ", data);
+    service[data[0]].apply(service, data.slice(1));
+  }
+}
+if (window.alkInfectURL)
+  alkDoInfect(alkInfectURL);
---
+++ vbuckets-vis.js
@@ -0,0 +1,344 @@
+var pendingMessages = [];
+var pendingHandler;
+var slaveFrame;
+
+console.log("masTER!");
+
+function onMessage(event) {
+  console.log("got event:", event);
+  if (event.data == "initial") {
+    slaveFrame = event.source;
+    slaveOrigin = event.origin;
+    return initialize();
+  }
+  if (pendingHandler) {
+    var handler = pendingHandler;
+    pendingHandler = null;
+    return handler(event);
+  }
+  pendingMessages.push(event);
+}
+
+window.addEventListener("message", onMessage, false);
+
+function BUG(msg) {
+  alert(msg);
+  debugger;
+  throw new Error(msg);
+}
+
+function waitMessage(body) {
+  if (pendingMessages.length) {
+    setTimeout(function () {
+      body(pendingMessages.shift);
+    }, 0);
+    return;
+  }
+  if (pendingHandler) {
+    BUG("unsupported waitMessage stacking!");
+  }
+  pendingHandler = body;
+}
+
+function sendAndWait(msg, body) {
+  if (!slaveFrame)
+    BUG("!slaveFrame");
+  waitMessage(function (event) {
+    body(event.data);
+  });
+  slaveFrame.postMessage(msg, slaveOrigin);
+}
+
+function slaveGet(url, body) {
+  var f = (function (reply, url) {
+    $.get(url, reply);
+  }).toString();
+  sendAndWait(["eval2", f, url], body);
+}
+
+// mostly stolen from MIT-licensed prototypejs.org (String#toQueryParams)
+function deserializeQueryString(dataString) {
+  return _.reduce(dataString.split('&'), { }, function(hash, pair) {
+    if ((pair = pair.split('='))[0]) {
+      var key = decodeURIComponent(pair.shift());
+      var value = pair.length > 1 ? pair.join('=') : pair[0];
+      if (value != undefined) value = decodeURIComponent(value);
+
+      if (key in hash) {
+        if (!_.isArray(hash[key]))
+          hash[key] = [hash[key]];
+        hash[key].push(value);
+      }
+      else hash[key] = value;
+    }
+    return hash;
+  })
+}
+
+function createSimpleElement(name) {
+  var dotpos = name.indexOf(".");
+  if (dotpos >= 0) {
+    var arr = name.split(".");
+    var rv = document.createElement(arr[0] || "div");
+    arr.shift();
+    rv.setAttribute('class', arr.join(" "));
+    return rv;
+  }
+  return document.createElement(name);
+}
+
+Object.extend = function (obj, attrs) {
+  for (var k in attrs) {
+    if (!attrs.hasOwnProperty(k))
+      continue;
+    obj[k] = attrs[k];
+  }
+  return obj;
+}
+
+var buildSetters = {
+  '@style': function (e, k, v) {
+    Object.extend(e.style, v);
+  },
+  '@eval': function (e, k, v) {
+    v(e);
+  }
+};
+
+function buildHTML(data) {
+  if (typeof(data) === "string")
+    return document.createTextNode(data);
+  if (data.nodeType !== undefined)
+    return data;
+  if (!(data instanceof Array)) {
+    BUG("expected text or array");
+  }
+  if (data[0] instanceof Function) {
+    return data[0].apply(null, data.slice(1));
+  }
+  var e = (data[0] instanceof Array) ? buildHTML(data[0]) : createSimpleElement(data[0]);
+  var childIdx = 1;
+  while (typeof(attrs = data[childIdx]) === 'object' && !(attrs instanceof Array)) {
+    childIdx++;
+    for (var k in attrs) {
+      if (!attrs.hasOwnProperty(k))
+        continue;
+      var setter = buildSetters[k];
+      if (setter) {
+        setter(e, k, attrs[k]);
+      } else {
+        e.setAttribute(k, attrs[k]);
+      }
+    }
+  }
+  while (childIdx < data.length) {
+    e.appendChild(buildHTML(data[childIdx++]));
+  }
+  return e;
+}
+
+
+var bucketName;
+
+function initialize() {
+  var body = document.body;
+  body.setAttribute("style", "white-space:pre;");
+  body.textContent = "ready!";
+  bucketName = "default";
+  (function () {
+    var match = /\?(.*?)(?:$|#)/.exec(window.location.href);
+    if (!match)
+      return;
+    var params = deserializeQueryString(match[1]);
+    if (params.bucket)
+      bucketName = params.bucket;
+  })();
+  updateData();
+}
+
+function updateData() {
+  var body = document.body;
+  slaveGet("/diag/vbuckets?bucket=" + encodeURIComponent(bucketName), function (data) {
+    body.innerHTML = '';
+    body.removeAttribute("style");
+    body.appendChild(buildHTML(["div.buttons",
+                                ["button", {"onclick": "less.refresh();"}, "refresh styles"],
+                                ["button", {"onclick": "updateData();"}, "refresh data"]]));
+    renderVBuckets(data);
+  })
+}
+
+function bestSquareSize(vbucketsNum) {
+  var maxAspect = 1.5;
+  var width = Math.ceil(Math.sqrt(vbucketsNum));
+  var bestRest = vbucketsNum;
+  var bestWidth;
+  var height;
+  // find best width so that 'packing' is tightest but within
+  // acceptable aspect
+  while (width/(height = Math.ceil(vbucketsNum / width)) <= maxAspect) {
+    var rest = vbucketsNum - height * width;
+    if (rest < bestRest) {
+      bestWidth = width;
+      bestRest = rest;
+    }
+    if (rest == 0)
+      break;
+    width++;
+  }
+  return bestWidth;
+}
+
+
+function colorLinear(rgb1, a1/*, rgb2, a2, ...*/) {
+  var i = 2;
+  var r=rgb1[0]*a1,g=rgb1[1]*a1,b=rgb1[2]*a1;
+  while (i < arguments.length) {
+    rgb1 = arguments[i++];
+    a1 = arguments[i++];
+    r += rgb1[0] * a1;
+    g += rgb1[1] * a1;
+    b += rgb1[2] * a1;
+  }
+  return "rgb(" + (r >> 0) + "," + (g>>0) + "," + (b>>0)+")";
+}
+
+var knownProperties = [];
+
+function nodeTable(name) {
+  return function (/*childs...*/) {
+    return buildHTML([
+      'p.node', {'data-node': name},
+      ['em', name],
+      ['table.node-table'].concat([].slice.call(arguments, 0)),
+      ['table.props-table'].concat((function () {
+        var props = knownProperties.slice(0).sort();
+        return [["tr", ["th", "property"], ["th", "value"]],
+                ["tr", ["td", "number"], ["td", {'data-property-name': 'number'}]]
+               ].concat(props.map(function (propertyName) {
+                 return ["tr", ["td", propertyName],
+                         ["td", {'data-property-name': propertyName}]]
+               }));
+      })())
+    ]);
+  }
+}
+
+var perNodeStates;
+var vbucketsNum;
+var copies;
+var bucketMap;
+
+function renderVBuckets(data) {
+  bucketMap = data.bucketMap;
+  perNodeStates = data.perNodeStates;
+  vbucketsNum = bucketMap.length;
+  copies = bucketMap[0].length;
+
+  var squareSize = bestSquareSize(vbucketsNum);
+
+  var nodes = Object.keys(perNodeStates).sort();
+
+  knownProperties = perNodeStates[nodes[0]]; // this is in fact vb -> stats mapping
+  knownProperties = Object.keys(knownProperties[Object.keys(knownProperties)[0]]);
+
+  nodes.forEach(function (node) {
+    function vbucketTD(n) {
+      if (!stats[n]) {
+        // vbucket is missing
+        return ["td.missing"];
+      }
+      var state = stats[n].state;
+      var pos = bucketMap[n].indexOf(node);
+      var attrs = {
+        '@style': {'backgroundColor': colorLinear([0, 255, 0], (copies - pos) / copies)},
+        "data-vb": n
+      };
+      var classes = (function () {
+        switch (state) {
+        case 'active':
+          if (pos !== 0) {
+            // active but map differs
+            return "active.bad";
+          } else {
+            return "active";
+          }
+        case 'replica':
+        case 'pending':
+          if (pos <= 0) {
+            // replica but map differs
+            return "replica.bad";
+          } else {
+            return "replica";
+          }
+        case 'dead':
+          return "dead";
+        default:
+          return "bad.missing";
+        }
+      })();
+      return ["td." + classes, attrs];
+    }
+    var stats = perNodeStates[node];
+    var tds = [];
+    for (var i = 0; i < vbucketsNum; i++) {
+      tds.push(vbucketTD(i));
+    }
+    var trs = [];
+    while (tds.length) {
+      trs.push(["tr"].concat(tds.slice(0, squareSize)));
+      tds = tds.slice(squareSize);
+    }
+    var nodeElement = buildHTML([nodeTable(node)].concat(trs));
+    document.body.appendChild(nodeElement);
+  });
+
+}
+
+(function () {
+  function filteringVB(body) {
+    return function (e) {
+      if (!('vb' in e.target.dataset))
+        return;
+      body(e, e.target.dataset.vb);
+    }
+  }
+
+  var delayedOnMouseBody;
+
+  function onMouse(e, vb) {
+    var method = (e.type == 'mouseover') ? 'add' : 'remove';
+
+    function body() {
+      var nodeList = document.querySelectorAll('.node-table td[data-vb="'+vb+'"]');
+      Array.prototype.forEach.call(nodeList, function (el) {
+        el.classList[method]('vb-current');
+      });
+
+      nodeList = Array.prototype.slice.call(document.querySelectorAll('.node'), 0);
+      nodeList.forEach(function (el) {
+        var nodeName = el.dataset.node;
+        if (!nodeName) BUG();
+        var stats = perNodeStates[nodeName][vb] || {};
+        stats.number = vb;
+        Array.prototype.forEach.call(el.querySelectorAll('td[data-property-name]'), function (td) {
+          td.textContent = (method === 'add') ? stats[td.dataset.propertyName] : '';
+        });
+      });
+    }
+
+    if (method == 'add') {
+      if (delayedOnMouseBody) {
+        delayedOnMouseBody();
+        delayedOnMouseBody = null;
+      }
+      body();
+    } else {
+      if (!delayedOnMouseBody)
+        delayedOnMouseBody = body;
+    }
+  }
+  onMouse = filteringVB(onMouse);
+  document.body.addEventListener('mouseover', onMouse, false);
+  document.body.addEventListener('mouseout', onMouse, false);
+})();
