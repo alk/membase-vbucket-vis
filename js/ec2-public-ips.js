ca14671f24531b07817343497c3e4ee3c3b0862e
Aliaksey Kandratsenka <alk@tut.by>
2011-10-06 12:09:05 +0300

    handle master node retrieval failure better

--- js/ec2-public-ips.js
+++ js/ec2-public-ips.js
@@ -6,7 +6,7 @@ function reloadData() {
   $('status-box').textContent = 'Ready!'
   InjectionController.slaveApply(onResult, function (reply) {
     // unfortunately due to bug in /diag/eval implementation we have to call eval from inside eval :(, thus extra escaping
-    var code = 'S = "{json, [rpc:call(N, erlang, apply, [fun () -> {struct, [{nodeName, N}, {masterNode, (try rpc:call(N, mb_master, master_node, []) catch _:_ -> unknown end)}, {publicIP, list_to_binary(os:cmd(\\"curl -s http://169.254.169.254/latest/meta-data/public-ipv4\\"))}]} end, []]) || N <- [node() | nodes()]]}.",' +
+    var code = 'S = "{json, [rpc:call(N, erlang, apply, [fun () -> {struct, [{nodeName, N}, {masterNode, (try rpc:call(N, mb_master, master_node, []) of {badrpc, _} -> unknown; X -> X catch _:_ -> unknown end)}, {publicIP, list_to_binary(os:cmd(\\"curl -s http://169.254.169.254/latest/meta-data/public-ipv4\\"))}]} end, []]) || N <- [node() | nodes()]]}.",' +
       '{value, V, _} = eshell:eval(S,[]), V.';
     $.post("/diag/eval", code, function (data, status) {
       reply(data, status);
